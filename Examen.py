82169de35d49b29fd950a088be9eb6e14dcc46c0
RPacheco <memer2433567@gmail.com>
2024-02-03 09:16:00 -0500

    examensito

--- Examen.py
+++ Examen.py
@@ -1,34 +1,26 @@
 #sistema de gestion  de biblioteca, debe de tener por lo menos dos clases, 
-#libro y  Biblioteca; libro debe de tener, titulo autorm año de publicacion, y disponibilidad del libro
+#libro y  Biblioteca; libro debe de tener, titulo, autor, año de publicacion, y disponibilidad del libro
 #ademas la clase libro debe de tener los siguientes metodos, prestar, devolver e informacion
 #biblioteca debe de tener la clase de libros y los metodos ; agregar libro, mostrar inventario y buscar libro
 
 variables = {}
 
 class biblioteca:
-    class libro:
-        def __init__(self, autor, ADP, DdelL):
-            self.autor = autor
-            self.ADP = ADP
-            self.DdelL = DdelL
-        
-        def exposicion(self):
-            print("Nombre del libro actual es: ", self.autor)
-            print("Año de Publicacion del Libro es: ",self.ADP)
-            print("Disponibilidad del Libro", self.DdelL)
-
 
     def agregar_libro(self):
         nombre = input("Ingrese el nombre del libro: ")
-        valor = input(f"Ingrese el año de publicacion de {nombre}: ")        
-        variables[nombre] = valor
-        print(f"Variable '{nombre}' agregada con valor '{valor}'\n")
+        fecha = (input(f"Ingrese el año de publicacion de {nombre}: "))
+        autor = input(f"Ingrese el autor de {nombre}: ")  
+        self.variables[nombre] = {"fecha" : fecha, "autor" : autor}
+        print("")
         
     
     def mostrar_inventario(self):
-        print("Variables disponibles:")
-        for nombre, valor in variables.items():
-            print("el libro" , {nombre}, "con fecha de creacion", {valor}," y esta disponible")      
+        print("Libros disponibles:")
+        for nombre, atributos in variables.items():
+            fecha = atributos["fecha"]
+            autor = atributos["autor"]
+            print(f"el libro {nombre} siendo creado por {autor} con fecha de creacion {fecha} y esta disponible\n")      
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 while True:                  
@@ -36,7 +28,6 @@ while True:
     print("Que Deseas hace?")
     print("agregar libro = 1")
     print("mostar libros = 2")
-    print("")
     print("salir = 0")
     eleccion=int(input(">"))
 
